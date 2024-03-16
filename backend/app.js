a65187198306db7ce4d90f37730e2914e1d0a6b9
maniksde <manik.98.chakraborty@gmail.com>
2024-03-16 08:51:37 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const route = require("./routes/route.js");
 const app = express();
-
+const PORT = 3000;
 app.use("/",route);
 
-app.listen(3000, ()=>{
-    console.log("ok");
-});
+const start = async()=> {
+    try{
+        app.listen(PORT, ()=>{
+            console.log(`connected with port ${PORT}`);
+        });
+    }catch(err){
+        console.log(err);
+    }
+}
+
+start();
